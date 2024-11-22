7e20d67013d87092891d8f09664365c4c20e5013
Adriana Huerta <huerta.adriana.2002@gmail.com>
2024-11-22 13:57:55 -0600

    FIREBALL

--- src/Final_Project.py
+++ src/Final_Project.py
@@ -15,6 +15,7 @@ class Particle():
         self.alpha = 255
         self.surface = self.update_surface()
 
+
     def update(self, dt):
         self.age += dt
         if self.age > self.life:
@@ -101,6 +102,37 @@ class Rain():
             trail.draw(surface)
 
 
+class Movement():
+    def __init__(self, wizardimg):
+        self.wizardimg = wizardimg
+        self.x = 500
+        self.y = 620
+
+
+    # def update(self, dt):
+
+    # loop through the list of pygame events, and check if the keys we care about are pressed down, then change the coordinates of the wizard accordingly
+
+    def draw(self, surface):
+        #self.wizardimg
+        surface.blit(self.wizardimg, (self.x,self.y))
+
+class Fireball():
+    def __init__(self, fireballimg, x):
+        self.fireballimg = fireballimg
+        self.x = x
+        self.y = 620
+        self.dead = False
+
+    def update(self):
+        self.y -= 25
+        # if off screen, set dead to true
+        if self.y < -375:
+            self.dead = True
+
+    def draw(self, surface):
+        surface.blit(self.fireballimg, (self.x,self.y))
+
 def main():
     pygame.init()    
     pygame.display.set_caption("Digital Rain")
@@ -108,21 +140,54 @@ def main():
     dt = 0
     screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
     resolution = pygame.display.get_window_size()
+    print(resolution)
     rain = Rain(resolution)
+    wizardimg = pygame.image.load('src/Wizard.png')
+    fireballimg = pygame.image.load('src/Fire.png')
+    Castle1 = pygame.image.load('src/Castle1.png')
+    Castle2 = pygame.image.load('src/Castle2.png')
+    wizard = Movement(wizardimg)
+    fireballs = []
     running = True
-    img = pygame.image.load('Wizard.png')
+    vel = 25
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            keys = pygame.key.get_pressed()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    fireball = Fireball(fireballimg, wizard.x)
+                    fireballs.append(fireball)
+
+        if keys[pygame.K_LEFT]:
+            wizard.x -= vel
+        if keys[pygame.K_RIGHT]:
+            wizard.x += vel
         rain.update(dt)
         maroon = pygame.Color(25, 0, 0)
         screen.fill(maroon)
         rain.draw(screen)
-        screen.blit(img, (500,500))
+        screen.blit(Castle1, (1300,620))
+        screen.blit(Castle2, (0,620))
+
+        for fireball in fireballs:
+            fireball.update()
+            fireball.draw(screen)
+        for i, fireball in enumerate(fireballs):
+            if fireball.dead:
+                del fireballs[i]
+        wizard.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(5)
         
+    #def update(self, dt):
+        #particle = Particle(self.pos, size=self.size, life=self.life)
+        #self.particles.insert(0, particle)
+        #self._update_particles(dt)
+        #self._update_pos()
+
     pygame.quit()
 
 if __name__ == "__main__":
